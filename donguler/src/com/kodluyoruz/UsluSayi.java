e2a14c98ba0c7a604f070e58ca511968129af6dc
Müge TAŞ <88267857+megitash@users.noreply.github.com>
2021-10-05 22:44:42 +0300

    eklenti

--- donguler/src/com/kodluyoruz/UsluSayi.java
+++ donguler/src/com/kodluyoruz/UsluSayi.java
@@ -1,2 +1,20 @@
-package com.kodluyoruz;public class UsluSayi {
+package com.kodluyoruz;
+
+import java.util.Scanner;
+
+public class UsluSayi {
+    public static void main(String[] args) {
+        int x, y, total=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("ussu alinacak sayi : ");
+        x= input.nextInt();
+
+        System.out.print("us : ");
+        y= input.nextInt();
+
+        for (int i=1; i<=y; i++){
+            total*=x;
+        }
+        System.out.println(total);
+    }
 }
