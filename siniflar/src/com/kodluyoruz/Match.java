b8c65b80bd6696184da90204e039653d027925c9
Müge TAŞ <88267857+megitash@users.noreply.github.com>
2021-10-11 20:33:13 +0300

    eklenti

---
+++ siniflar/src/com/kodluyoruz/Match.java
@@ -0,0 +1,59 @@
+package com.kodluyoruz;
+
+import java.awt.*;
+
+public class Match {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+
+    Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.maxWeight = maxWeight;
+        this.minWeight = minWeight;
+
+    }
+
+    void run() {
+        if (isCheck()) {
+            while (this.f1.health > 0 && this.f2.health > 0) {
+                System.out.println("====YENI RAUND====");
+                this.f2.health = this.f1.hit(f2);
+                if (isWin()) {
+                    break;
+                }
+
+                this.f1.health = this.f2.hit(f1);
+                if (isWin()) {
+                    break;
+                }
+                System.out.println(this.f1.name + " Saglik : " + this.f1.health);
+                System.out.println(this.f2.name + " Saglik : " + this.f2.health);
+            }
+
+        } else {
+            System.out.println("Sporcularin sikletleri uymuyor.");
+        }
+    }
+
+    boolean isCheck() {
+        return (this.f1.weight >= minWeight && this.f1.weight <= maxWeight && this.f2.weight >= minWeight && this.f2.weight <= maxWeight);
+
+    }
+
+    boolean isWin() {
+        if (this.f1.health == 0) {
+            System.out.println(this.f2.name + " kazandi!");
+            return true;
+        }
+        if (this.f2.health == 0) {
+            System.out.println(this.f1.name + " kazandi!");
+            return true;
+        }
+        return false;
+    }
+}
+
